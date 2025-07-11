98232c0c09baae7184f025a44717c0425fd869b1
Rohit_Patel_IIITR <127647959+rohit-iiitr@users.noreply.github.com>
2025-07-11 15:26:01 +0530

    Update Login.tsx

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { BACKEND_URL } from "../utils/utils";
 import axios from "axios";
 import { toast } from "react-toastify";
